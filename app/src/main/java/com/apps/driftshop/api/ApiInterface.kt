1580bf287f93c71c0473ced45a8a16e6f6c27074
Peter Ndumia <ndegwandumia@gmail.com>
2023-04-19 14:01:48 +0300

    Feat: Add Api instance and product data class

---
+++ app/src/main/java/com/apps/driftshop/api/ApiInterface.kt
@@ -0,0 +1,27 @@
+package com.apps.driftshop.api
+
+import com.apps.driftshop.model.Product
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiInterface {
+    @GET("products")
+    suspend fun getProducts():List<Product>
+
+    companion object{
+        var apiInstance:ApiInterface? =null
+        fun getInstance() : ApiInterface{
+            if (apiInstance == null){
+                val retrofit =Retrofit.Builder()
+                    .baseUrl("https://api.drift.co.ke/api/test/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+                apiInstance = retrofit.create((ApiInterface::class.java))
+
+            }
+            return apiInstance!!
+        }
+    }
+}
